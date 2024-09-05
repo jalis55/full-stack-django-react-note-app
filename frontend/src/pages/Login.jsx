9df29fa6976ddc7930e51685a0d7cbe937bd3d67
Jalis Mahamud Tarif <jalis.tarif@fintechhub.com.bd>
2024-09-05 13:30:31 +0600

    updated route protection

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,20 @@
-import Form from "../components/Form"
+
+import { Navigate } from 'react-router-dom';
+import Form from '../components/Form';
+import useCheckAuthorized from '../CheckAuthorized';  // Updated import to use the custom hook
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    const isAuthorized = useCheckAuthorized();  // Use the custom hook
+
+    if (isAuthorized === null) {
+        return <div>Loading...</div>;  // Show a loading indicator while checking authorization
+    }
+
+    if (isAuthorized) {
+        return <Navigate to="/" />;  // Redirect to home page if authorized
+    }
+
+    return <Form route="/api/token/" method="login" />;  // Show login form if not authorized
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import Form from '../components/Form';
 
+import { Navigate } from 'react-router-dom';
+import Form from '../components/Form';
+import useCheckAuthorized from '../CheckAuthorized';  
 
 const Register = () => {
-  return  <Form route="/api/user/register/" method="register" />
+  const isAuthorized = useCheckAuthorized();  // Use the custom hook
+
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;  // Show a loading indicator while checking authorization
+  }
+
+  if (isAuthorized) {
+    return <Navigate to="/" />;  // Redirect to home page if authorized
+  }
+  return <Form route="/api/user/register/" method="register" />
 }
 
 export default Register;
